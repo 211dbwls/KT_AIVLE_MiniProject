eb5b5f1ef2a2918029fdae2debcea7317503f75e
211dbwls <65964035+211dbwls@users.noreply.github.com>
2023-06-19 22:32:40 +0900

    #Project01 로그아웃 구현

--- Project01/mysite/accounts/urls.py
+++ Project01/mysite/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Project01/mysite/accounts/views.py
+++ Project01/mysite/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm, UserCreationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
@@ -28,3 +28,8 @@ def login_view(request):
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+    
