3158d65eca6b43093586f83e49a97676e348be82
211dbwls <65964035+211dbwls@users.noreply.github.com>
2023-06-20 22:16:00 +0900

    #Project01 회원가입 구현

--- Project01/mysite/accounts/forms.py
+++ Project01/mysite/accounts/forms.py
@@ -10,13 +10,10 @@ class LoginForm(forms.Form):
 # 회원가입폼
 class SignUpForm(UserCreationForm):
     username = forms.CharField(label='Username')
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password_check = forms.CharField(label='Check Password', widget=forms.PasswordInput)
-    first_name = forms.CharField(label='First Name')
-    last_name = forms.CharField(label='Last Name')
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Check Password', widget=forms.PasswordInput)
     email = forms.EmailField(label='Email Address')
-    phone_number = forms.CharField(label='Phone number', max_length=11)
     
     class Meta:
         model = User
-        fields = ['username', 'password', 'password_check', 'first_name', 'last_name', 'email', 'phone_number']
+        fields = ['username', 'password1', 'password2', 'email']
--- Project01/mysite/accounts/views.py
+++ Project01/mysite/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm, SignUpForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -11,9 +12,10 @@ def login_view(request):
     if request.user.is_authenticated: 
         return redirect('index') 
     
+    signup_form = SignUpForm()
+    
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
-        signup_form = SignUpForm()
         if login_form.is_valid():
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
@@ -25,7 +27,6 @@ def login_view(request):
                 messages.warning(request, "로그인 정보가 올바르지 않습니다.")
     else:
         login_form = LoginForm()
-        signup_form = SignUpForm()
 
     return render(request, 'login.html', {'login_form': login_form, 'signup_form':signup_form})
 
@@ -36,5 +37,18 @@ def logout_view(request):
 
 # 회원가입
 def signup(request):
-    return render(request, 'login.html')
+    login_form = LoginForm()
+    
+    if request.method == 'POST':
+        signup_form = SignUpForm(request.POST)
+        if signup_form.is_valid():
+            signup_form.save()
+            return redirect('index')
+        else:
+            print(signup_form.errors)
+            messages.warning(request, '회원가입에 실패했습니다.')
+    else:
+        signup_form = SignUpForm()
+    
+    return render(request, 'login.html', {'login_form': login_form, 'signup_form':signup_form})
     
